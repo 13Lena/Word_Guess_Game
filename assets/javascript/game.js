3b778e98eef445d89f8e58e91339f1378f67abc3
Lena Hamilton 13 <hamiltonlena21@gmail.com>
2019-03-03 18:46:32 -0700

    everything

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,111 @@
 //inisilize game 
 
-
 //pick random word from list
 var wordList = [
-    "black", "eyeliner",
-    "leather", "vampire",
-    "victorian", "pastel",
-    "corporate", "platforms",
-    "corset", "parasol",
-    "silver", "fishnet",
-    "skull", "bat",
-    "lipstick", "nocturnal",
-    "", "",
-    "", "",
+  "black", "eyeliner",
+  "leather", "vampire",
+  "victorian", "pastel",
+  "corporate", "platforms",
+  "corset", "parasol",
+  "silver", "fishnet",
+  "skull", "bat",
+  "lipstick", "nocturnal"
 ];
 
+var answerWord = [];
+var winCounter = 0;
+
 var hiddenWord = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(hiddenWord)
 
-
-//show empty spaces
-var answerArray = [ ];
-
-for (var i = 0; i < hiddenword.length; i++) {
- answerArray[i] = "_";
+//word to guess
+for (var i = 0; i < hiddenWord.length; i++) {
+  answerWord[i] = ("_");
 }
 
-var remainingLetters = hiddenword.length;
+// wrong array
+var wrongGuessedLetters = new Array(10);
+for (var j = 0; j <= 9; j++) {
+  wrongGuessedLetters[j] = ("_");
+}
 
-//when letter is guessed
-   // keep track (print out for player) & make sure its legal
-    //compare to word, show if matching 
-    // put in wrong letter array otherwise
-var guessLetter;
-var wrongGuessedLetters = [ ];
-    wrongGuessedLetters.length = 9;
-    
-//loop through everytime, like question game
-    //set number of loops (10), display
-    //end when wrong guess array full
-    while (remainingLetters > 0) {
+//how empty spaces =========================
+function documentPushes() {
+  document.getElementById("answerWord").innerText = answerWord.join(' ');
+  debugger
+  document.getElementById("wrongGuessedLetters").innerText = wrongGuessedLetters
+    .join(' ');
 
+  // win counter
+  document.getElementById("winCounter").innerText = winCounter;
 
+}
+function newWord () {
+  answerWord = [];
+  hiddenWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-    }
+  //word to guess
+for (var i = 0; i < hiddenWord.length; i++) {
+  answerWord[i] = ("_");
+}
 
-//if all letters are guessed, esccape/win
-    //if loops run out/lose
-    var showLives = document.getElementById("mylives");
-      // Show lives
-   comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-      showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === geusses.length) {
-        showLives.innerHTML = "You Win!";
-      }
+// wrong array
+wrongGuessedLetters = new Array(10);
+for (var j = 0; j <= 9; j++) {
+  wrongGuessedLetters[j] = ("_");
+}
+
+}
+documentPushes();
+
+// when letter is guessed
+//    keep track (print out for player) & make sure its legal
+//     compare to each letter on word,  if matching 
+//     put in wrong letter array otherwise
+// function wrongLettersFull( ) 
+//   escape = lose;
+//   return true
+// loop through everytime, like question game
+//     set number of loops (10), display
+//    end when wrong guess array full
+
+//main game ===================================================
+
+document.onkeyup = function (event) {
+  var guessedLetter = event.key;
+  console.log(guessedLetter);
+  debugger
+
+  var found = false;
+  for (var j = 0; j < answerWord.length; j++) {
+    if (hiddenWord[j] === guessedLetter) {
+      answerWord[j] = guessedLetter;
+      found = true;
     }
   }
+  debugger;
+  // function wrongGuess() 
+  if (!found) {
+    var shownWrongLetters = wrongGuessedLetters.indexOf("_");
+    wrongGuessedLetters[shownWrongLetters] = guessedLetter;
+      
+  }
+  if (wrongGuessedLetters[9] != "_") {
+    newWord();
+  }
+  
+  if (answerWord.join('') === hiddenWord) {
+    winCounter++;
+    newWord();
+  }
 
-//record/display number of wins
-    //automaticly restart after loss or win
+  documentPushes();
+
+  //location.reload();
+  // Is key in the hiddenWord
+  // var isKeyInHiddenWord = hiddenWord.indexOf(guessedLetter) !== -1
+
+  // console.log(isKeyInHiddenWord, hiddenWord);
+
+  }
+
